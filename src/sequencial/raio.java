9a69a5c67fa70133336ac035271955297752e826
Mathias Coletti <mathiasc2004@gmail.com>
2023-12-10 16:03:47 -0300

    raio

--- src/sequencial/raio.java
+++ src/sequencial/raio.java
@@ -1,5 +1,18 @@
 package sequencial;
 
+import java.util.Scanner;
+
 public class raio {
+    public static void main(String[] args) {
+        double raio, comprimento;
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Insira seu raio");
+        raio = sc.nextDouble();
+
+        comprimento = 2 * Math.PI * Math.pow(raio,2);
+
+        System.out.println("A saida dos dados é: " + comprimento);
+    }
 
 }
