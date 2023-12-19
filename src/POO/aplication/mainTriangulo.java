a0459c51c73b8c80bbd162d5d103bbd813031180
MathiasColetti <mathiasc2004@gmail.com>
2023-12-19 16:48:43 -0300

    Teste

---
+++ src/POO/aplication/mainTriangulo.java
@@ -0,0 +1,8 @@
+package POO.aplication;
+
+public class mainTriangulo {
+    public static void main(String[] args) {
+        Triangulo t1 = new Triangulo(10,20,30);
+        System.out.println(t1.getArea());
+    }
+}
--- src/String/main.java
+++ src/String/main.java
@@ -2,25 +2,26 @@ package String;
 
 public class main {
 
-    public  static void comparaValores(float a, float b){
-        if (a> b){
+    public static void comparaValores(float a, float b) {
+        if (a > b) {
             System.out.println("O numero " + a + " é maior que " + b);
-        } else if (a< b) {
+        } else if (a < b) {
             System.out.println("O numero " + b + " é maior que " + a);
         } else {
             System.out.println("São iguais");
         }
     }
+
     public static void main(String[] args) {
         /*Paciencia*/
         String nome = "-  Mathias  -";
 
         System.out.println(nome.toUpperCase());
-        System.out.println(nome.replace("M","n")); // Replace
+        System.out.println(nome.replace("M", "n")); // Replace
         System.out.println(nome.trim());
 
 
-        comparaValores(3,2);
+        comparaValores(3, 2);
 
     }
 }
